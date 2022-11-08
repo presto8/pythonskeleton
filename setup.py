24e5f638ffb264eba1194812b87223f1caea3d90
Preston Hunt <me@prestonhunt.com>
2022-11-08 09:44:29 -0800

    bump Python version to 3.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pythonskeleton',
     version='0.1',
-    python_requires='>=3.7',
+    python_requires='>=3.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
